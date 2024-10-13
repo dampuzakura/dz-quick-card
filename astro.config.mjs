1d5126ef4a5740f7eb5a25d9e001699a8987e4fd
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-10-13 21:15:23 +0900

    🔧 ベースURLの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dampuzakura.github.io',
+  base: '/dz-quick-card',
   integrations: [svelte()]
 });
